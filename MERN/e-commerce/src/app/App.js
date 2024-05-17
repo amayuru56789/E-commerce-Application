05b190848ba05d4b62b0234cfd41e375c3a0c04a
Amayuru Indeewara <amayuru_i@epic.lk>
2024-05-18 02:03:50 +0530

    added nav-bar component and config tailwind css

--- MERN/e-commerce/src/app/App.js
+++ MERN/e-commerce/src/app/App.js
@@ -1,11 +1,14 @@
 // import logo from './logo.svg';
-// import './App.css';
+// import '../App.css';
+
+// import NavBar from "../components/NavBar";
 
 function App() {
   return (
     <div className="App">
+     
       <header className="App-header">
-       <h1>Amayuru indeewara</h1>
+       <h1 className='text-6xl text-red-500 bg-purple-200'>Amayuru indeewara</h1>
       </header>
     </div>
   );
