c9570cdc364b591b1d0296a6f8eefcf3388ce2b5
Amayuru Indeewara <amayuru_i@epic.lk>
2024-05-18 23:37:26 +0530

    added product images using cloudinary

--- MERN/e-commerce/backend/index.js
+++ MERN/e-commerce/backend/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 
+const product = require("./product")
+
 const app = express();
 
 app.use(express.json());
@@ -10,8 +12,8 @@ app.get("/", (req, res) => {
     res.send("Welcome to our online shop API...");
 });
 
-app.get("/products", (req, res) => {
-    res.send([2, 3, 4]);
+app.get("/product", (req, res) => {
+    res.send(product);
 });
 
 const port = process.env.PORT || 5000;
---
+++ MERN/e-commerce/backend/product.js
@@ -0,0 +1,25 @@
+const product = [
+    {
+        id: 1,
+        name: "iPhone 12 Pro",
+        desc: "6.1-inch display",
+        price: 999,
+        image: "https://res.cloudinary.com/dwpn8ckhh/image/upload/v1716055196/onlineShop/iphone12pro_msndv7.jpg"
+    },
+    {
+        id: 2,
+        name: "iPhone 12",
+        desc: "5.4-inch display",
+        price: 699,
+        image: "https://res.cloudinary.com/dwpn8ckhh/image/upload/v1716055196/onlineShop/iphone12_rxvybl.jpg"
+    },
+    {
+        id: 3,
+        name: "Huawei HONOR",
+        desc: "6.5-inch display",
+        price: 399,
+        image: "https://res.cloudinary.com/dwpn8ckhh/image/upload/v1716055197/onlineShop/1686481639_nkppvf.jpg"
+    }        
+];
+
+module.exports = product;
