1ebcd06d0a01b4ec13ff52cfaa9f00a8ff2f3d6a
Amayuru Indeewara <amayuru_i@epic.lk>
2024-05-19 01:00:46 +0530

    configure redux-toolkit and added axios dependency

--- MERN/e-commerce/src/index.js
+++ MERN/e-commerce/src/index.js
@@ -4,10 +4,24 @@ import './index.css';
 import App from '././app/App';
 import reportWebVitals from './reportWebVitals';
 
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+
+import ProductReducer from "./features/productSlice";
+
+const store = configureStore({
+  reducer: {
+    product: ProductReducer
+  },
+});
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
+   
   </React.StrictMode>
 );
 
